c6125aa06ee9948675cc396e1c15af842d340dec
MananDhinora <dhinoramanan@gmail.com>
2025-11-29 00:09:30 +0530

    created the task model and registered the app

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    due_date = models.DateField()
+    estimated_hours = models.PositiveIntegerField()
+    importance = models.PositiveIntegerField()
+
+    dependencies = models.ManyToManyField(
+        "self", symmetrical=False, related_name="dependents"
+    )
+
+    def __str__(self):
+        return self.title
